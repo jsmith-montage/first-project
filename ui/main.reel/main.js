126af2683305778a90ab5978c101dc59b6cf8a74
jsmith-montage <noreply>
2015-01-06 03:33:02 +0000

    Update component ui/main.reel/main.js and file index.html

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -14,6 +14,16 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             this.super();
         }
     },
+    
+    init: {
+        value: function() {
+            //Not as clean to define property descriptors.....
+            //Gets default getter and setters
+            //Can't define writable, configurable, enumerable unless calling defineProperty()?
+            this.defaultValue = "World";
+        }
+    },
+    
     defaultValue: {
         value: "World"
     }
