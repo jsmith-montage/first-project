f3a32992b35b0489e4f4966977822d2faa024a6d
jsmith-montage <noreply>
2015-01-06 21:11:47 +0000

    Add component simpleform

---
+++ ui/simpleform.reel/simpleform.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/simpleform.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Simpleform
+ * @extends Component
+ */
+exports.Simpleform = Component.specialize(/** @lends Simpleform# */ {
+    constructor: {
+        value: function Simpleform() {
+            this.super();
+        }
+    }
+});
