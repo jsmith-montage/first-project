da1229e4f29b4a0742d8e1df89103eef2f193e65
jsmith-montage <noreply>
2015-01-07 01:43:12 +0000

    Add component map

---
+++ ui/map.reel/map.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/map.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Map
+ * @extends Component
+ */
+exports.Map = Component.specialize(/** @lends Map# */ {
+    constructor: {
+        value: function Map() {
+            this.super();
+        }
+    }
+});
