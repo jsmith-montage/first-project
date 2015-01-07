8cb9f7f8aa55faed62a00a943a12e908f20f5167
jsmith-montage <noreply>
2015-01-07 01:41:20 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -27,8 +27,8 @@ exports.Main = Component.specialize(/** @lends Main# */ {
 
     handleDecrementAction: {
         value: function (event) {
-            var value = Number(this.templateObjects.value.element.textContent);
-            this.templateObjects.value.element.textContent = value-1;
+            var value = Number(this.templateObjects.value.value);
+            this.templateObjects.value.value = value-1;
         }
     }
 
